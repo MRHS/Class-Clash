87f928feccf173f73d41e06ebb6f7b4340c59b09
spocot <spencerkocot@gmail.com>
2013-03-12 12:50:23 -0400

    Outlines Update

---
+++ src/com/github/spocot/kwest/ImagesPlayerWatcher.java
@@ -0,0 +1,5 @@
+package com.github.spocot.kwest;
+
+public interface ImagesPlayerWatcher {
+	void sequenceEnded(String imageName);
+}
---
+++ src/com/github/spocot/kwest/RobotOutline.java
@@ -0,0 +1,46 @@
+package com.github.spocot.kwest;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public abstract class RobotOutline {
+	
+	//Robot life
+	private int life = 100;
+	
+	//Movement speed of the robot
+	private int speed = 1;
+	
+	//Name of the robot
+	private String name;
+	
+	//Coords for robot
+	private int x;
+	private int y;
+	
+	//X and Y Variation
+	private int xVar = 0;
+	private int yVar = 0;
+	
+	//ArrayList of all other robots
+	private ArrayList<RobotOutline> robots;
+	
+	public RobotOutline(ArrayList<RobotOutline> robots, int x, int y){
+		this.robots = robots;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setSpeed(int speed){
+		this.speed = speed;
+	}
+	
+	public void update(){
+		x += xVar * speed;
+		y += yVar * speed;
+	}
+	
+	public void render(Graphics g){
+		
+	}
+}
