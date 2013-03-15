cd8559fcab658a7e6bd5b3a4d529ae818f4c988f
spocot <spencerkocot@gmail.com>
2013-03-15 11:59:28 -0400

    Updated comments in Location class

--- src/class_clash/Location.java
+++ src/class_clash/Location.java
@@ -1,23 +1,29 @@
 package class_clash;
 
 public class Location {
+	
+	//X and Y variables for a specific location
 	private int x;
 	private int y;
 	
+	//Constructor
 	public Location(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 	
+	//Update a location
 	public void update(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
 	
+	/** Return the X-coord of the location */
 	public int getX(){
 		return x;
 	}
 	
+	/** Return the Y-coord of the location */
 	public int getY(){
 		return y;
 	}
